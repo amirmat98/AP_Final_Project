af5aac334c45d185b4892036721434dc1808d043
amirmat98 <matin.amirmahdi@gmail.com>
2019-05-25 18:44:10 +0430

    update

---
+++ cmake-build-debug/CMakeFiles/phase_1.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/phase_1.dir/main.cpp.o"
+  "CMakeFiles/phase_1.dir/Core.cpp.o"
+  "CMakeFiles/phase_1.dir/Input_Handler.cpp.o"
+  "CMakeFiles/phase_1.dir/User.cpp.o"
+  "CMakeFiles/phase_1.dir/Publisher.cpp.o"
+  "CMakeFiles/phase_1.dir/Customer.cpp.o"
+  "CMakeFiles/phase_1.dir/Film.cpp.o"
+  "CMakeFiles/phase_1.dir/Comment.cpp.o"
+  "CMakeFiles/phase_1.dir/Message.cpp.o"
+  "CMakeFiles/phase_1.dir/Money_Handler.cpp.o"
+  "CMakeFiles/phase_1.dir/Error_Handler.cpp.o"
+  "CMakeFiles/phase_1.dir/Parameter_Handler.cpp.o"
+  "phase_1.pdb"
+  "phase_1"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/phase_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
